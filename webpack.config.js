de2c8c096b2e3e4bc938dcda0538277b0161a475
Yury Lugantsov <lugantsov@irt.de>
2019-09-23 15:24:58 +0200

    some small changes

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/deploy-test/' 
+      : '/',
     filename: 'build.js'
   },
   module: {
