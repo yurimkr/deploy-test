4a658cb279fd2cc0b63a1ebeee50b4e0539c6567
Yury Lugantsov <lugantsov@irt.de>
2019-09-26 13:21:03 +0200

    test other value for publicPath

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -17,7 +17,7 @@ module.exports = {
   output: {
     filename: `${PATHS.assets}js/[name].[hash].js`,
     path: PATHS.dist,
-    publicPath: '/'
+    // publicPath: '/'
   },
   module: {
     rules: [
